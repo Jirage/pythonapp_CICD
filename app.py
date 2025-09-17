d7076f721b3a828c3b8ef2ea0da2d2dc49331e8c
Jirage <abhishekjirage98@gmail.com>
2025-09-17 14:37:39 +0530

    Modified Jenkinsfile

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return 'successfully deployed python application through jenkins!!!!!!!!!, added webhook on Jirage Github'
+    return 'successfully deployed python application through jenkins!!!!!!!!!, added Abhishek Github'
 @app.route('/hi')
 def hell():
     return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
